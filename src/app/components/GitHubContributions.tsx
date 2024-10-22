c14e49a0846853ae73ad217dbc81227170d1645d
veroni1337 <aliserb773@gmail.com>
2024-10-22 23:09:53 +0200

    Added github contributions block

---
+++ src/app/components/GitHubContributions.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import GitHubCalendar from 'react-github-calendar';
+
+const GitHubContributions = ({ username }) => {
+  return (
+    <div className="github_contributions">
+      <div className="container mx-auto p-8">
+        <h2>My Github contributions</h2>
+
+        <div className="github_contributions_component mt-12">
+          <GitHubCalendar
+            username={username}
+            colorScheme="dark"
+            fontSize={14}
+            blockSize={15}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default GitHubContributions;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import GitHubContributions from "./components/GitHubContributions";
+
 export default function Home() {
   return (
     <div className="home_page_wrapper">
-
+      <GitHubContributions username="Aliserb" />
     </div>
   );
 }
