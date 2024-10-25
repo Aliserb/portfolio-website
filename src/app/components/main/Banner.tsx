44577132f58907ab363875d5ec73062daf27c6ce
Alisher <aliserb773@gmail.com>
2024-10-25 14:43:14 +0200

    created banner component

---
+++ src/app/components/main/Banner.tsx
@@ -0,0 +1,13 @@
+const Banner = () => {
+    return (
+        <div className="banner_section">
+            <div className="container">
+                <div className="banner_row">
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Banner;
